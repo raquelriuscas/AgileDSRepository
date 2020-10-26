20334930864b174d0ff70323fd62f7f6f5e42210
raquelrius <raquelrius@hotmail.com>
2020-10-26 22:49:48 +0100

    adding flask app2

---
+++ settings.py
@@ -0,0 +1,4 @@
+MONGO_DBNAME = 'foodb'
+MONGO_URI = "mongodb+srv://raquelrius:DBlyy7dT9fjHqwbA@cluster0.nkpgc.mongodb.net/foodb"
+SECRET_KEY = 'enydM2ANhdcoKwdVa0jWvEsbPFuQpMjf'
+SESSION_PROTECTION = 'strong'
